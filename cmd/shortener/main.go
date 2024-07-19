1133180d49ead10124a4da0255e5f0e040e69178
a_turundaev <a.turundaev@etpgpb.ru>
2024-07-19 14:51:42 +0300

    +Logger

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"bytes"
 	"flag"
+	"fmt"
 	"github.com/arturturundaev/shorturl/internal/app/handler"
 	"github.com/arturturundaev/shorturl/internal/app/repository/localstorage"
 	"github.com/arturturundaev/shorturl/internal/app/service"
 	"github.com/arturturundaev/shorturl/internal/config"
+	ginzap "github.com/gin-contrib/zap"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"io"
 	"net/http"
 	"os"
+	"time"
 )
 
 func main() {
@@ -26,11 +33,42 @@ func main() {
 
 	router := gin.Default()
 
+	addLogger(router)
+
 	router.POST(`/`, handlerSave.Handle)
 	router.GET(`/:short`, handlerFind.Handle)
 
+	fmt.Println(serverConfig.AddressStart.String())
 	err := http.ListenAndServe(serverConfig.AddressStart.String(), router)
 	if err != nil {
 		panic(err)
 	}
+}
+
+func addLogger(r *gin.Engine) {
+	logger, _ := zap.NewProduction()
+
+	r.Use(ginzap.Ginzap(logger, time.RFC3339, true))
+	r.Use(ginzap.RecoveryWithZap(logger, true))
+	r.Use(ginzap.GinzapWithConfig(logger, &ginzap.Config{
+		UTC:        true,
+		TimeFormat: time.RFC3339,
+		Context: ginzap.Fn(func(c *gin.Context) []zapcore.Field {
+			var fields []zapcore.Field
+			// log request ID
+			if requestID := c.Writer.Header().Get("X-Request-Id"); requestID != "" {
+				fields = append(fields, zap.String("request_id", requestID))
+			}
+
+			// log request body
+			var body []byte
+			var buf bytes.Buffer
+			tee := io.TeeReader(c.Request.Body, &buf)
+			body, _ = io.ReadAll(tee)
+			c.Request.Body = io.NopCloser(&buf)
+			fields = append(fields, zap.String("body", string(body)))
+
+			return fields
+		}),
+	}))
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/arturturundaev/shorturl
 
 go 1.22.4
 
-require github.com/stretchr/testify v1.9.0
+require (
+	github.com/gin-gonic/gin v1.10.0
+	github.com/stretchr/testify v1.9.0
+)
 
 require (
 	github.com/bytedance/sonic v1.11.9 // indirect
@@ -12,7 +15,7 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.4 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/gin-gonic/gin v1.10.0 // indirect
+	github.com/gin-contrib/zap v1.1.3 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.22.0 // indirect
@@ -27,6 +30,8 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/arch v0.8.0 // indirect
 	golang.org/x/crypto v0.24.0 // indirect
 	golang.org/x/net v0.26.0 // indirect
